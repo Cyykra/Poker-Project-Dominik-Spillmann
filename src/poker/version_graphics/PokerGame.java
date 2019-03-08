9565ba6b7d6fafc0d44cabbeb95844446d981190
Cyykra <dominik.spillmann@students.fhnw.ch>
2019-03-08 13:23:40 +0100

    Pictures

--- src/poker/version_graphics/PokerGame.java
+++ src/poker/version_graphics/PokerGame.java
@@ -6,6 +6,7 @@ import poker.version_graphics.controller.PokerGameController;
 import poker.version_graphics.model.PokerGameModel;
 import poker.version_graphics.view.PokerGameView;
 
+
 public class PokerGame extends Application {
 	public static final int NUM_PLAYERS = 2;
 	PokerGameModel model;
